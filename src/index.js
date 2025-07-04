2cd46611300f0822850d11574092548f737224d4
selim737 <33039541+selim737@users.noreply.github.com>
2025-07-04 11:53:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PromptViewer from './components/PromptViewer';
-import CustomFieldEditor from './components/CustomFieldEditor';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PromptViewer from "./components/PromptViewer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>PersonaSync is live!</h1>
-      <PromptViewer />
-      <hr style={{ margin: '2rem 0' }} />
-      <CustomFieldEditor />
-    </div>
+    <PromptViewer />
   </React.StrictMode>
 );
