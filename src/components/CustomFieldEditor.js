f6faf9e38539a59705ef59e789771df7a00ca314
selim737 <33039541+selim737@users.noreply.github.com>
2025-07-04 13:14:45 +0200

    Update CustomFieldEditor.js

--- src/components/CustomFieldEditor.js
+++ src/components/CustomFieldEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function CustomFieldEditor({ fields, setFields }) {
   const [keyInput, setKeyInput] = useState('');
