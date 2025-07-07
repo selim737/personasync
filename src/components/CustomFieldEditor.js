e1ca69fb2cf7fadadeadf8b920a9654e354a9315
selim737 <33039541+selim737@users.noreply.github.com>
2025-07-07 10:21:03 +0200

    Update CustomFieldEditor.js

--- src/components/CustomFieldEditor.js
+++ src/components/CustomFieldEditor.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "./ui/card";
 
 function CustomFieldEditor({ fields, setFields }) {
   const [keyInput, setKeyInput] = useState('');
