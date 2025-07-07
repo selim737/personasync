78fa5bb7e2c24851dbee6cf10bc7dfacf48cb490
selim737 <33039541+selim737@users.noreply.github.com>
2025-07-07 10:28:11 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonaEditor from './components/PersonaEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonaEditor />
+    </div>
+  );
+}
+
+export default App;
