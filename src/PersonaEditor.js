bc28cb8a49f606f9e7e7114a5fc27ee74756c7aa
selim737 <33039541+selim737@users.noreply.github.com>
2025-07-08 11:49:32 +0200

    Update PersonaEditor.js

--- src/PersonaEditor.js
+++ src/PersonaEditor.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import React from 'react';
 import YAML from "yaml";
 import { Card, CardContent } from "./components/ui/card";
 import { Input } from "./components/ui/input";
